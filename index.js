d25001224c932f06c6a50ecc19a3a4401810d595
Ujlaky Tibor <tujlaky@adverticum.com>
2015-05-27 14:57:42 +0200

    module fix

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
-var getContent = require('./modules/get_content');
+var path         = require('path');
+var childProcess = require('child_process');
+var phantomjs    = require('phantomjs');
+var binPath      = phantomjs.path;
+var Promise      = require('bluebird');
 
-getContent('http://redrouge.hu')
-	.then(function(content) {
-		console.log(content);
-	})
-	.catch(function(err) {
-		throw err;
+var getContent = function(url) {
+	return new Promise(function(resolve, reject) {
+		var childArgs = [
+			path.join(__dirname, 'get_content_phantom.js'),
+			url
+		];
+
+		var execFile = Promise.promisify(childProcess.execFile);
+
+		execFile(binPath, childArgs)
+			.then(function(execOut) {
+				var result;
+				var stdout = execOut[0] || '';
+
+				try {
+					result = JSON.parse(stdout);
+					resolve(result);
+				} catch(parseError) {
+					reject(parseError);
+				}
+			})
+			.catch(function(err) {
+				reject(err);
+			});
 	});
+};
+
+module.exports = getContent;
