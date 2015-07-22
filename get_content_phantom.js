b3a167f3d17c714e8e0e08a2dfadbff1304307ec
Ujlaky Tibor <tujlaky@adverticum.com>
2015-07-22 12:41:35 +0200

    indent fix

--- get_content_phantom.js
+++ get_content_phantom.js
@@ -9,7 +9,7 @@ if ( system.args.length < 2 ) {
 var url = system.args[1];
 
 page.onConsoleMessage = function (msg) {
-	//console.log(msg);
+  //console.log(msg);
 };
 
 page.open(url, function (status) {
@@ -17,65 +17,65 @@ page.open(url, function (status) {
     console.log('Unable to access network');
   } else {
     var content = page.evaluate(function() {
-		var tags = document.body.getElementsByTagName('*');
-		var notAllowed = ['SCRIPT', 'LINK'];
-		var content = [];
+      var tags = document.body.getElementsByTagName('*');
+      var notAllowed = ['SCRIPT', 'LINK'];
+      var content = [];
 
-		var titleNodes = document.getElementsByTagName('title');
+      var titleNodes = document.getElementsByTagName('title');
 
-		var title = '';
+      var title = '';
 
-		if ( titleNodes && titleNodes.length > 0 ) {
-			title = titleNodes[0].text;
-		}
+      if ( titleNodes && titleNodes.length > 0 ) {
+        title = titleNodes[0].text;
+      }
 
-		var metas = document.getElementsByTagName('meta');
+      var metas = document.getElementsByTagName('meta');
 
-		var searchedMetas = ['description', 'keywords'];
+      var searchedMetas = ['description', 'keywords'];
 
-		var pageMetas = {};
+      var pageMetas = {};
 
-		for ( var j=0; j<metas.length; j++ ) {
-			var meta = metas[j];
+      for ( var j=0; j<metas.length; j++ ) {
+        var meta = metas[j];
 
-			var name = meta.getAttribute('name');
+        var name = meta.getAttribute('name');
 
-			if ( name && searchedMetas.indexOf(name) !== -1 ) {
-				pageMetas[name] = meta.getAttribute('content');
-			}
+        if ( name && searchedMetas.indexOf(name) !== -1 ) {
+          pageMetas[name] = meta.getAttribute('content');
+        }
 
-		}
+      }
 
-		for ( var i=0; i<tags.length; i++ ) {
-			var tag = tags[i];
+      for ( var i=0; i<tags.length; i++ ) {
+        var tag = tags[i];
 
-			if ( (tag.text) && (notAllowed.indexOf(tag.tagName) === -1) ) {
-				var parents = [];
+        if ( (tag.text) && (notAllowed.indexOf(tag.tagName) === -1) ) {
+          var parents = [];
 
-				var parentNode = tag.parentNode || null;
+          var parentNode = tag.parentNode || null;
 
-				while (parentNode && parentNode.tagName !== 'HTML' && parentNode.tagName !== 'BODY') {
-					parents.push(parentNode.tagName);
-          parentNode = parentNode.parentNode || null;
-				}
+          while (parentNode && parentNode.tagName !== 'HTML' && parentNode.tagName !== 'BODY') {
+            parents.push(parentNode.tagName);
+            parentNode = parentNode.parentNode || null;
+          }
 
-				content.push({
-					tag: tag.tagName,
-					text: tag.text,
-					parents: parents
-				});
-			}
-		}
+          content.push({
+            tag: tag.tagName,
+            text: tag.text,
+            parents: parents
+          });
+        }
+      }
 
 
-		return {
-			title: title,
-			metas: pageMetas,
-			content: content
-		};
+      return {
+        title: title,
+        metas: pageMetas,
+        content: content
+      };
     });
 
-	console.log(JSON.stringify(content));
+    console.log(JSON.stringify(content));
   }
 
   phantom.exit();
--- index.js
+++ index.js
@@ -5,32 +5,32 @@ var binPath      = phantomjs.path;
 var Promise      = require('bluebird');
 
 var getContent = function(url) {
-	return new Promise(function(resolve, reject) {
-		var childArgs = [
-			path.join(__dirname, 'get_content_phantom.js'),
-			url
-		];
+  return new Promise(function(resolve, reject) {
+    var childArgs = [
+      path.join(__dirname, 'get_content_phantom.js'),
+      url
+    ];
 
-		var execFile = Promise.promisify(childProcess.execFile);
+    var execFile = Promise.promisify(childProcess.execFile);
 
-		execFile(binPath, childArgs)
-			.then(function(execOut) {
-				var result;
-				var stdout = execOut[0] || '';
+    execFile(binPath, childArgs)
+      .then(function(execOut) {
+        var result;
+        var stdout = execOut[0] || '';
 
-				try {
-					result = JSON.parse(stdout);
-					resolve(result);
-				} catch(parseError) {
-					reject(parseError);
-				}
-			})
-			.catch(function(err) {
-				reject(err);
-			});
-	});
+        try {
+          result = JSON.parse(stdout);
+          resolve(result);
+        } catch(parseError) {
+          reject(parseError);
+        }
+      })
+    .catch(function(err) {
+      reject(err);
+    });
+  });
 };
 
 module.exports = {
-	getContent: getContent
+  getContent: getContent
 };
